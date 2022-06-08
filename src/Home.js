e707c15a6a41c0c843083896d4ef14ba163d7f3c
Christen Weston <paleeserecycle@gmail.com>
2022-06-07 19:58:46 -0700

    Use State Examples

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
-const Home = () => {
+import { useState } from 'react';
 
-  const handleClick = (e) => {
-    console.log('Hello folks!', e);
+const Home = () => {
+  const [name, setName] = useState('Mario');
+  const [age, setAge] = useState(35);
+  const handleClick = () => {
+    setName('luigi');
+    setAge(30);
   }
 
   const handleClickAgain = (name, e) => {
     console.log('hello ' + name, e.target);
   }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
       <button onClick={(e) => handleClickAgain('Mario', e) }>Click me again</button>
     </div>
