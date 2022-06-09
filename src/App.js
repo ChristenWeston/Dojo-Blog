db8a98a753323a0268d070044c24e36680c6d998
Christen Weston <paleeserecycle@gmail.com>
2022-06-08 20:39:23 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import Navbar from "./Navbar";
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-
-
   return(
-    <div className="App">
-      <Navbar />
-     <div className="content">
-       <Home />
-
-     </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home />}>
+                </Route>
+              <Route path="/create" element={<Create />}>
+                </Route>
+            </Routes>
+          </div>
+      </div>
+    </Router>
   );
 }
 
