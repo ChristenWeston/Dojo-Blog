16bc91bb587786294dda2814b4e7c79c0e77e5ee
Christen Weston <paleeserecycle@gmail.com>
2022-06-08 19:15:44 -0700

    useEffect initial fun

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(35);
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
+  const [name, setName] = useState('mario');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
+// Use effect runs after every render. Second value passed in is the dependency. So, it will only run if the dependency changes
+  useEffect(() => {
+    console.log('use effect ran')
+    console.log(name)
+  }, [name]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('Mario', e) }>Click me again</button>
+      {/* //blogs is a prop */}
+        <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+        <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
+        <button onClick={() => setName('Luigi')}>change name</button>
+        <p> { name }</p>
     </div>
   );
 }
